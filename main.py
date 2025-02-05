f4147b04eb231f3184c54b3370338dcfef2fd74c
LucSchimidt <llourencoschimidt@gmail.com>
2025-02-05 17:36:07 -0300

    setup with openai api

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import requests
+import os
+import dotenv
+from openai import OpenAI
 
-def getGptResponse():
-    return "Olá mundo"
+dotenv.load_dotenv()
+
+def getGptResponse(content: str):
+
+    client = OpenAI(
+    api_key=os.getenv("OPENAI_KEY")
+    )
+
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    store=True,
+    messages=[
+        {"role": "developer", "content": "voce ira receber um conteudo de resumo de uma matéria, seu papel será formular oito perguntas e respostas baseadas nesse conteúdo"},
+        {"role": "developer", "content": "voce irá gerar cada um dos pares pergunta resposta em um json com a chave question e chave answer, cada json estará dentro de um array principal com todas perguntas e respostas."},
+        {
+            "role": "user",
+            "content": f"Conteúdo: {content}"
+        }
+    ]
+    )
+
+    print(completion.choices[0].message);
+
 
 if __name__ == "__main__":
-    print(getGptResponse())
+    getGptResponse("Segunda guerra mundial.")
